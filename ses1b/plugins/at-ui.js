b359c366e15fec2e40078cab8b962fcfe714c2be
Sheng <shengzengg@gmail.com>
2018-10-13 21:37:07 +1100

    got amplify to work

--- ses1b/plugins/at-ui.js
+++ ses1b/plugins/at-ui.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import AtComponent from 'at-ui'
 import 'at-ui-style' // import css
 
-//import ui-style or import the unbuilt version of SCSS
+import Amplify, * as AmplifyModules from 'aws-amplify';
+import { AmplifyPlugin } from 'aws-amplify-vue';
+import aws_exports from './aws-exports';
+Amplify.configure(aws_exports);
 
+Vue.use(AmplifyPlugin, AmplifyModules);
+
+
+//import ui-style or import the unbuilt version of SCSS
 Vue.use(AtComponent)
