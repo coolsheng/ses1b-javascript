fbb86310c9b8866e99a8de90089821ae06f4ef59
Sheng <shengzengg@gmail.com>
2018-10-15 00:54:02 +1100

    test

--- ses1b/pages/profile.vue
+++ ses1b/pages/profile.vue
@@ -175,45 +175,6 @@ data () {
 
 <style scoped>
 
-  .container {
-  margin-top: 10px;
-  display: 100px; 
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  }
-
-
-  .alignSteps {
-  margin-left: 75px;
-  }
- 
-  .block{
-  padding:10px; 
-  float:left; 
-  }
-  .clear{
-    clear: both;
-  }
-  .intro-font{
-  padding:10px; 
-  float:left;   
-  font-weight: 300;font-size: 20px;color: #35495e;
-  letter-spacing: 1px; text-decoration: underline;
-  text-decoration-color: rgb(219, 59, 59) ; 
-  text-underline-position: under;
-  }
-  .input-field{
-   width: 240px;
-   margin-left: 30px
-  }
- .textarea {
- width: 365px; 
- height: 150px;
- padding-left:17px;
- padding-top:10px;
- resize: none;
-}
 
 
 </style>
