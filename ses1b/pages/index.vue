8ebe48eccc159b10e85bba39ac643be5f4845310
Sheng <shengzengg@gmail.com>
2018-10-14 22:14:46 +1100

    fixed bug

--- ses1b/pages/index.vue
+++ ses1b/pages/index.vue
@@ -2,40 +2,39 @@
   <section class="container">
     <div>
       <app-logo/>
+        <div v-show="register">
+          <p>Enter your credentials below</p>
+        <section class="textContainer" >
+        <at-input v-model="registerUsername" placeholder="Username"></at-input> 
+        </section>
 
         <section class="textContainer">
-        <at-input v-model="inputValue" placeholder="Username"></at-input> 
+        <at-input v-model="registerPassword" type="password" placeholder="Password"></at-input>
         </section>
+        </div>
+
+         <div v-show="!register">
+        <section class="textContainer" >
+        <at-input v-model="username" placeholder="Username"></at-input>         </section>
 
         <section class="textContainer">
         <at-input v-model="password" type="password" placeholder="Password"></at-input>
         </section>
+        </div> 
 
         <section class="label">
-        <label for="remember"> <input id="remember" type="checkbox"> Remember me </label>
+        <at-button type="text" @click="register = true" v-show="!register">Register for an account</at-button>    
         </section>
 
         <div class="links">
-         <nuxt-link to="/profile"> 
-          <at-button size="large">
-            <section class="lastResort">Login</section></at-button>
-         </nuxt-link>
-                  <nuxt-link to="/register"> 
-          <at-button size="large">
-            <section class="lastResort">Register</section></at-button>
-         </nuxt-link>
+          <!-- <nuxt-link to="/profile">  -->
+          <at-button size="large" v-if="!register">
+            <section class="lastResort" @click="checkLogin()">Login</section></at-button>
+         <!-- </nuxt-link> -->
+          <at-button size="large" v-if="register" @click="submitRegister()" > Register</at-button>
         </div>
         
-        <section style="margin-top: 9px" class="textContainer">
-        <p style="font-family:Quicksand">─────── or LOG IN WITH ───────</p>
-        </section>
 
-        <script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script>
-
-        <meta name="google-signin-client_id" content="79211080622-dvn6hr7esbil545oabc8fn01k5lmq4os.apps.googleusercontent.com">
-        
-        <div style="margin-top: 12px; display: inline-block;"  id="google-signin-btn">
-        </div>
 
     </div>
   </section>   
@@ -49,18 +48,31 @@ export default {
     components: {
     AppLogo
 },
-    methods: {
-    onSignIn (user) {
-            const profile = user.getBasicProfile()
-          this.$Message.success('Signed in successfully as ' + profile.getName())
-      console.log('Name: ' + profile.getName());
-      
-    }
+data(){
+  return{
+    password: "",
+    username: "",
+    registerUsername: "",
+    registerPassword: "",
+    register: false,    
+  }
 },
-  mounted() {
-    gapi.signin2.render('google-signin-btn', { // this is the button "id"
-      onsuccess: this.onSignIn // note, no "()" here
-    })
+    methods: {
+    submitRegister(){
+      this.register = false; 
+      this.$Message.success('Successfully registered')
+      
+      console.log(this.registerPassword, this.registerUsername)
+    },
+    checkLogin(){
+      if(this.registerPassword == this.password && this.registerUsername == this.username && this.password != ""){
+        window.location.href = "/message";
+      }
+      else{
+          this.$Message.error('Login failed: please check your username and password are correct')
+       }
+},
+
   }
 }
 </script>
