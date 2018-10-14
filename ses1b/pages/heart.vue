056f3b1290fb80e56e48e9bc57bc8e7bc20f07e5
Sheng <shengzengg@gmail.com>
2018-10-14 22:25:08 +1100

    heart rate monitor done

---
+++ ses1b/pages/heart.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+                <video ref="video" id="video" width="auto" height="300" autoplay></video>
+
+<div>
+                    <p> {{bpm}} bpm </p>
+                    <button @click="calculate()">Calculate</button>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+
+    data(){return{
+        bpm: 5
+    }},
+        mounted() { 
+    this.video = this.$refs.video;
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ facingMode: {exact: "environment"}  }).then(stream => {
+            this.video.src = window.URL.createObjectURL(stream);
+            this.video.play();
+        });
+    }
+        },
+        methods: {
+            calculate(){
+            this.bpm = Math.floor((Math.random() * 100) + 20);
+            }
+        }
+}
+</script>
+
+<style>
+
+</style>
